86eab8835359c51099925b0e12902eac03f2dab3
Sven Anders <sven@anders-hamburg.de>
2025-12-28 09:10:05 +0100

    Add MBus Address field to meter

--- librelandlord/bill/models/meter.py
+++ librelandlord/bill/models/meter.py
@@ -9,6 +9,13 @@ from .meter_place import MeterPlace
 
 class Meter(models.Model):
 
+    class RemoteType(models.TextChoices):
+        MANUAL = 'manual', _('Manual')
+        MBUS = 'mbus', _('M-Bus')
+        MODBUS = 'modbus', _('Modbus')
+        HTTP = 'http', _('HTTP API')
+        MQTT = 'mqtt', _('MQTT')
+
     # Define fields for the Meter model
     place = models.ForeignKey(
         MeterPlace, on_delete=models.CASCADE, verbose_name=_("Meter Place"))
@@ -22,6 +29,22 @@ class Meter(models.Model):
         verbose_name=_("Calibrated until"))
     out_of_order_date = models.DateField(
         null=True, blank=True, verbose_name=_("Out of Order Date"))
+
+    remote_type = models.CharField(
+        max_length=10,
+        choices=RemoteType.choices,
+        default=RemoteType.MANUAL,
+        verbose_name=_("Remote Type")
+    )
+
+    remote_address = models.CharField(
+        max_length=200,
+        blank=True,
+        default='',
+        verbose_name=_("Remote Address"),
+        help_text=_(
+            "Address/ID for remote reading (e.g. M-Bus serial, IP address, API endpoint)")
+    )
 
     class Meta:
         constraints = [

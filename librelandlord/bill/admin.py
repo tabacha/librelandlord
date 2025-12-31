8c57bb6a6ba8189dca7f371c70f969734445dae1
Sven Anders <sven@anders-hamburg.de>
2025-12-31 18:04:15 +0100

    Django Admin angepasst

--- librelandlord/bill/admin.py
+++ librelandlord/bill/admin.py
@@ -6,22 +6,47 @@ from . import models
 
 class MeterPlaceAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'remark', 'location')
-    search_fields = ['name', 'location', 'type']
+    list_filter = ['type']
+    search_fields = ['name', 'location', 'remark']
+    ordering = ['name']
 
 
 admin.site.register(models.MeterPlace, MeterPlaceAdmin)
 
 
 class MeterAdmin(admin.ModelAdmin):
-    list_display = ('place', 'remark', 'meter_number',
-                    'build_in_date', 'out_of_order_date')
+    list_display = ('meter_number', 'place', 'remark', 'remote_type',
+                    'build_in_date', 'calibrated_until_date', 'out_of_order_date')
+    list_filter = ['place__type', 'remote_type',
+                   'build_in_date', 'out_of_order_date']
+    search_fields = ['meter_number', 'remark', 'place__name', 'remote_address']
+    autocomplete_fields = ['place']
+    date_hierarchy = 'build_in_date'
+    ordering = ['-build_in_date']
+    fieldsets = (
+        (None, {
+            'fields': ('place', 'meter_number', 'remark')
+        }),
+        ('Dates', {
+            'fields': ('build_in_date', 'calibrated_until_date', 'out_of_order_date')
+        }),
+        ('Remote Reading', {
+            'fields': ('remote_type', 'remote_address'),
+            'classes': ('collapse',)
+        }),
+    )
 
 
 admin.site.register(models.Meter, MeterAdmin)
 
 
 class MeterReadingAdmin(admin.ModelAdmin):
-    list_display = ('meter', 'date', 'meter_reading')
+    list_display = ('date', 'meter', 'meter_reading', 'time')
+    list_filter = ['date', 'meter__place']
+    search_fields = ['meter__meter_number', 'meter__place__name']
+    autocomplete_fields = ['meter']
+    date_hierarchy = 'date'
+    ordering = ['-date', '-time']
 
 
 admin.site.register(models.MeterReading, MeterReadingAdmin)
@@ -48,6 +73,7 @@ class ConsumptionCalcAdmin(admin.ModelAdmin):
     list_filter = ['operator', 'start_date']
     search_fields = ['name']
     inlines = [ConsumptionCalcArgumentInline]
+    ordering = ['name']
 
     # Blende die alten deprecated Felder aus
     exclude = [
@@ -82,14 +108,23 @@ admin.site.register(models.ConsumptionCalc, ConsumptionCalcAdmin)
 
 
 class AccountEntryAdmin(admin.ModelAdmin):
-    list_display = ('date', 'text', 'value', 'bill')
+    list_display = ('date', 'text', 'value', 'bill', 'account_period')
+    list_filter = ['date', 'account_period', 'bill__cost_center']
+    search_fields = ['text', 'bill__text', 'bill__bill_number']
+    autocomplete_fields = ['bill', 'account_period']
+    date_hierarchy = 'date'
+    ordering = ['-date']
 
 
 admin.site.register(models.AccountEntry, AccountEntryAdmin)
 
 
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ('number', 'name', 'street', 'size_in_m2')
+    list_display = ('number', 'name', 'street',
+                    'postal_code', 'city', 'size_in_m2')
+    list_filter = ['city']
+    search_fields = ['number', 'name', 'street', 'city', 'postal_code']
+    ordering = ['number']
 
 
 admin.site.register(models.Apartment, ApartmentAdmin)
@@ -97,14 +132,61 @@ admin.site.register(models.Apartment, ApartmentAdmin)
 
 class RenterAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'apartment',
-                    'move_in_date', 'move_out_date')
+                    'move_in_date', 'move_out_date', 'is_active')
+    list_filter = ['apartment', 'move_in_date', 'move_out_date']
+    search_fields = ['first_name', 'last_name',
+                     'apartment__number', 'apartment__name']
+    autocomplete_fields = ['apartment']
+    date_hierarchy = 'move_in_date'
+    ordering = ['-move_in_date']
+
+    def is_active(self, obj):
+        """Zeigt an, ob der Mieter aktuell aktiv ist"""
+        return obj.move_out_date is None
+    is_active.boolean = True
+    is_active.short_description = "Active"
 
 
 admin.site.register(models.Renter, RenterAdmin)
 
 
 class BillAdmin(admin.ModelAdmin):
-    list_display = ('text', 'bill_date', 'value', 'from_date', 'to_date')
+    list_display = ('formatted_bill_date', 'text', 'bill_number', 'value',
+                    'cost_center', 'account_period', 'formatted_from_date', 'formatted_to_date')
+    list_filter = ['bill_date', 'cost_center', 'account_period']
+    search_fields = ['text', 'bill_number', 'cost_center__text']
+    autocomplete_fields = ['cost_center', 'account_period']
+    date_hierarchy = 'bill_date'
+    ordering = ['-bill_date']
+    fieldsets = (
+        (None, {
+            'fields': ('text', 'bill_number', 'bill_date', 'value')
+        }),
+        ('Assignment', {
+            'fields': ('cost_center', 'account_period')
+        }),
+        ('Period', {
+            'fields': ('from_date', 'to_date')
+        }),
+    )
+
+    def formatted_bill_date(self, obj):
+        """Zeigt bill_date im Format DD.MM.YYYY"""
+        return obj.bill_date.strftime('%d.%m.%Y')
+    formatted_bill_date.short_description = 'Bill Date'
+    formatted_bill_date.admin_order_field = 'bill_date'
+
+    def formatted_from_date(self, obj):
+        """Zeigt from_date im Format DD.MM.YYYY"""
+        return obj.from_date.strftime('%d.%m.%Y')
+    formatted_from_date.short_description = 'From Date'
+    formatted_from_date.admin_order_field = 'from_date'
+
+    def formatted_to_date(self, obj):
+        """Zeigt to_date im Format DD.MM.YYYY"""
+        return obj.to_date.strftime('%d.%m.%Y')
+    formatted_to_date.short_description = 'To Date'
+    formatted_to_date.admin_order_field = 'to_date'
 
 
 admin.site.register(models.Bill, BillAdmin)
@@ -112,41 +194,97 @@ admin.site.register(models.Bill, BillAdmin)
 
 class CostCenterAdmin(admin.ModelAdmin):
     list_display = ('text', 'is_oiltank')
+    list_filter = ['is_oiltank']
+    search_fields = ['text']
+    ordering = ['text']
 
 
 admin.site.register(models.CostCenter, CostCenterAdmin)
 
 
 class CostCenterContributionAdmin(admin.ModelAdmin):
-    list_display = ('cost_center', 'apartment', 'consumption_calc')
+    list_display = ('get_display_name', 'cost_center', 'apartment',
+                    'special_designation', 'consumption_calc')
+    list_filter = ['cost_center', 'apartment']
+    search_fields = ['apartment__number', 'apartment__name',
+                     'special_designation', 'cost_center__text',
+                     'consumption_calc__name']
+    autocomplete_fields = ['cost_center', 'apartment', 'consumption_calc']
+    ordering = ['cost_center', 'apartment']
+
+    def get_display_name(self, obj):
+        """Zeigt den Anzeigenamen"""
+        return obj.get_display_name()
+    get_display_name.short_description = "Name"
 
 
 admin.site.register(models.CostCenterContribution, CostCenterContributionAdmin)
 
 
 class CostCenterBillEntryAdmin(admin.ModelAdmin):
-    list_display = ('cost_center', 'account_entry', 'oil_in_liter')
+    list_display = ('cost_center', 'account_entry',
+                    'oil_in_liter', 'get_value')
+    list_filter = ['cost_center']
+    search_fields = ['cost_center__text', 'account_entry__text']
+    autocomplete_fields = ['cost_center', 'account_entry']
+    ordering = ['cost_center']
+
+    def get_value(self, obj):
+        """Zeigt den Wert aus dem AccountEntry"""
+        return obj.account_entry.value
+    get_value.short_description = "Value"
 
 
 admin.site.register(models.CostCenterBillEntry, CostCenterBillEntryAdmin)
 
 
 class AccountPeriod(admin.ModelAdmin):
-    list_display = ('text', 'start_date', 'end_date')
+    list_display = ('text', 'start_date', 'end_date', 'duration_days')
+    list_filter = ['start_date', 'end_date']
+    search_fields = ['text']
+    date_hierarchy = 'start_date'
+    ordering = ['-start_date']
+
+    def duration_days(self, obj):
+        """Berechnet die Dauer der Periode in Tagen"""
+        delta = obj.end_date - obj.start_date
+        return delta.days
+    duration_days.short_description = "Duration (days)"
 
 
 admin.site.register(models.AccountPeriod, AccountPeriod)
 
 
 class HeatingInfo(admin.ModelAdmin):
-    list_display = ('apartment', 'year', 'month')
+    list_display = ('apartment', 'year', 'month', 'heating_energy_kwh',
+                    'hot_water_energy_kwh', 'hot_water_m3')
+    list_filter = ['year', 'month', 'apartment']
+    search_fields = ['apartment__number', 'apartment__name']
+    autocomplete_fields = ['apartment']
+    ordering = ['-year', '-month', 'apartment']
 
 
 admin.site.register(models.HeatingInfo, HeatingInfo)
 
 
 class HeatingInfoTemplate(admin.ModelAdmin):
-    lists_display = ('apartment')
+    list_display = ('apartment', 'next_year', 'next_month',
+                    'calc_heating', 'calc_hot_water', 'kwh_per_m3_hot_water')
+    list_filter = ['next_year', 'next_month']
+    search_fields = ['apartment__number', 'apartment__name']
+    autocomplete_fields = ['apartment', 'calc_heating', 'calc_hot_water']
+    ordering = ['apartment']
+    fieldsets = (
+        (None, {
+            'fields': ('apartment', 'next_year', 'next_month')
+        }),
+        ('Calculations', {
+            'fields': ('calc_heating', 'calc_hot_water', 'kwh_per_m3_hot_water')
+        }),
+        ('Comparison Groups', {
+            'fields': ('compare_heating_group', 'compare_hot_water_group')
+        }),
+    )
 
 
 admin.site.register(models.HeatingInfoTemplate, HeatingInfoTemplate)

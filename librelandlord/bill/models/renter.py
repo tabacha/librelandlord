c6c4711231c45f34ecc92eb3fc94748d2fcd6418
Sven Anders <sven@anders-hamburg.de>
2026-01-01 13:26:16 +0100

    Owner Occupied Renter (#34)

--- librelandlord/bill/models/renter.py
+++ librelandlord/bill/models/renter.py
@@ -22,6 +22,11 @@ class Renter(models.Model):
     move_in_date = models.DateField(verbose_name=_("Move in on date"))
     move_out_date = models.DateField(
         null=True, blank=True, verbose_name=_("Move out date"))
+    is_owner_occupied = models.BooleanField(
+        default=False,
+        verbose_name=_("Owner occupied"),
+        help_text=_("Check if this unit is occupied by the owner or family members. "
+                    "These are not considered rental income for tax purposes."))
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.apartment}"
